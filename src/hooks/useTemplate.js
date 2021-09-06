4e5ac5a82ba4d6bd44729f22e2cf430050ce9149
TuLT <tult@rikkeisoft.com>
2021-09-06 22:25:08 +0700

    fix: the attributes displaying wrong when delete a attribute

--- src/hooks/useTemplate.js
+++ src/hooks/useTemplate.js
@@ -34,9 +34,12 @@ const reducer = (state, action) => {
     case TEMPLATE_ACTIONS.ADD_ATTRIBUTE:
       return {
         ...state,
-        attributes: [...state.attributes, { id: state.attributes.length + 1 }],
+        attributes: [...state.attributes, { id: state.attributes.length > 0 ? state.attributes.pop().id + 1 : 1 }],
         variations: state.variations.map((variation) => {
-          return { ...variation, attributes: [...state.attributes, { id: state.attribute.length + 1 }] }
+          return {
+            ...variation,
+            attributes: [...state.attributes, { id: state.attributes.length > 0 ? state.attributes.pop().id + 1 : 1 }],
+          }
         }),
       }
     case TEMPLATE_ACTIONS.SET_ATTRIBUTE:
