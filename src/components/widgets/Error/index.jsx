6c1f370e3f0e53c28fbe655ac0a18a216e0457d5
TuLT <tult@rikkeisoft.com>
2021-09-18 01:45:54 +0700

    fix: error notification is not wide enough

--- src/components/widgets/Error/index.jsx
+++ src/components/widgets/Error/index.jsx
@@ -3,7 +3,7 @@ import Icon from 'components/elements/Icon'
 
 const Error = ({ error }) => {
   return (
-    <div className="flex flex-col items-center justify-center center-modal w-96 -translate-x-7/12 left-7/12">
+    <div className="flex flex-col items-center justify-center center-modal -translate-x-7/12 left-7/12">
       <Icon name="sadFace" style="w-16 h-16" fill="#ed3737" />
       <p className="w-full text-red-600">{error.errors?.message || 'Something went wrong, please re-try later'}</p>
     </div>
