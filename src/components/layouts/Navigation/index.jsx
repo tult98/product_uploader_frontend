5ed6a6e31fa8d3568183292423787f4250ccd10b
TuLT <tult@rikkeisoft.com>
2021-09-18 01:35:35 +0700

    fix: better switching page response

--- src/components/layouts/Navigation/index.jsx
+++ src/components/layouts/Navigation/index.jsx
@@ -7,18 +7,18 @@ import { GENERAL_ROUTES, PRODUCT_ROUTES, TEMPLATE_ROUTES } from 'routes'
 const Navigation = () => {
   return (
     <div className="fixed flex flex-col w-1/6 h-full bg-gray-800 text-white100">
-      <div className="navigation-item">
+      <Link to={GENERAL_ROUTES.HOME} className="navigation-item">
         <Icon name="home" style="w-12 h-12 mr-4" fill={colors.white100} />
-        <Link to={GENERAL_ROUTES.HOME}>Home</Link>
-      </div>
-      <div className="navigation-item">
+        Home
+      </Link>
+      <Link to={TEMPLATE_ROUTES.LIST_TEMPLATE} className="navigation-item">
         <Icon name="template" style="w-12 h-12 mr-4" fill={colors.white100} />
-        <Link to={TEMPLATE_ROUTES.LIST_TEMPLATE}>Template</Link>
-      </div>
-      <div className="navigation-item">
+        Template
+      </Link>
+      <Link to={PRODUCT_ROUTES.LIST_PRODUCT} className="navigation-item">
         <Icon name="product" style="w-12 h-12 mr-4" fill={colors.white100} />
-        <Link to={PRODUCT_ROUTES.LIST_PRODUCT}>Product</Link>
-      </div>
+        Product
+      </Link>
     </div>
   )
 }
